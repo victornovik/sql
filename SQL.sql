8169caa8dace9abb738b7e45fbe063cfb0036440
victornovik <victor.novik@mail.ru>
2026-02-05 12:50:24 +0300

    Moved all types of GROUP BY to SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -574,3 +574,39 @@ actual_prices as (
   where not exists (select 1 from s_item_price t where t.item_id = pr.item_id and t.actual_date > pr.actual_date)
 )
 select a.item_id, address, price from actual_addresses a join actual_prices p on a.item_id = p.item_id
+
+
+-- Classic GROUP BY
+SELECT Country, Region, SUM(sales) AS TotalSales
+FROM Sales
+GROUP BY Country, Region;
+
+-- Groups with subtotals and totals
+SELECT Country, Region, SUM(sales) AS TotalSales
+FROM Sales
+GROUP BY ROLLUP (Country, Region);
+
+-- Groups for all combinations of Country and Region
+SELECT Country, Region, SUM(sales) AS TotalSales
+FROM Sales
+GROUP BY CUBE (Country, Region);
+
+-- Returns the union of the ROLLUP and CUBE results for Country and Region
+SELECT Country, Region, SUM(Sales) AS TotalSales
+FROM Sales
+GROUP BY GROUPING SETS ( ROLLUP (Country, Region), CUBE (Country, Region) );
+
+
+--CREATE TABLE Sales ( 
+--Country VARCHAR(50), 
+--Region VARCHAR(50), 
+--Sales INT );
+
+--INSERT INTO sales VALUES (N'Canada', N'Alberta', 100);
+--INSERT INTO sales VALUES (N'Canada', N'Columbia', 200);
+--INSERT INTO sales VALUES (N'Canada', N'Columbia', 300);
+--INSERT INTO sales VALUES (N'Canada', N'Columbia', 300);
+--INSERT INTO sales VALUES (N'Canada', N'Alberta', 100);
+--INSERT INTO sales VALUES (N'United States', N'Montana', 100);
+--INSERT INTO sales VALUES (N'Columbia', N'Columbia', 300);
+--INSERT INTO sales VALUES (N'Columbia', N'Center', 150);
